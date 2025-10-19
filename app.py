359932b65907cc935d852d1a4afa41f42d7b7c0c
Axel <axelwarriorrenteria@gmail.com>
2025-10-19 01:27:28 -0600

    modificacion nombre dispercion / dispersion

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
-scatter_button = st.button('Construir grafico de dispercion') # crear un botón
+scatter_button = st.button('Construir grafico de dispersion') # crear un botón
 
 if scatter_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un grafico de dispercion para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
     
-    # crear un grafico de dispercion
+    # crear un grafico de dispersion
     fig = px.scatter(car_data, x="odometer")
 
     # mostrar un gráfico Plotly interactivo
